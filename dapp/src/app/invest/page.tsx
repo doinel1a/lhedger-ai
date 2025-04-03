25912be22d0dacce18f8f61aa11cb9717722fdca
doinel1a <doinel1atanasiu@gmail.com>
2025-04-03 14:33:45 +0200

    chore: create invest & portfolio pages

---
+++ dapp/src/app/invest/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function InvestPage() {
+  return (
+    <div className='flex h-full w-full items-center justify-center'>
+      <h1 className='text-2xl'>Invest page</h1>
+    </div>
+  );
+}
---
+++ dapp/src/app/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PortfolioPage() {
+  return (
+    <div className='flex h-full w-full items-center justify-center'>
+      <h1 className='text-2xl'>Portfolio page</h1>
+    </div>
+  );
+}
