b687352dfe8233f04d624865b6e5f1e1a3ef2c9c
Alexe Spataru <alexeluca.spataru@gmail.com>
2025-04-05 01:53:24 +0300

    fix smart contracts

--- contracts/src/Bundler.sol
+++ contracts/src/Bundler.sol
@@ -1,14 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.28;
 
-import { ISwapRouter } from "@uniswap/v3-periphery/interfaces/ISwapRouter.sol";
+import { IV3SwapRouter } from "@uniswap/router/interfaces/IV3SwapRouter.sol";
 import { IQuoterV2 } from "@uniswap/v3-periphery/interfaces/IQuoterV2.sol";
+import { IERC20Metadata } from "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract Bundler {
     IERC20 public USDC = IERC20(0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913);
-    ISwapRouter public UNI_V3_Router = ISwapRouter(0x2626664c2603336E57B271c5C0b26F421741e481);
+    IV3SwapRouter public UNI_V3_Router = IV3SwapRouter(0x2626664c2603336E57B271c5C0b26F421741e481);
     IQuoterV2 public UNI_V3_Quoter = IQuoterV2(0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a);
+
+    mapping(address user => address[] tokens) private userTokens;
+
+    struct UserTokens {
+        address token;
+        uint8 decimals;
+        uint256 balance;
+    }
 
     struct SwapUniV3 {
         address tokenOut;
@@ -36,27 +45,41 @@ contract Bundler {
         }
     }
 
-    function executeBatchSwaps(uint256 amountIn, SwapUniV3[] calldata swaps) public {
-        USDC.transferFrom(msg.sender, address(this), amountIn);
+    function executeBatchSwaps(uint256 totalAmount, SwapUniV3[] calldata swaps) public {
+        USDC.transferFrom(msg.sender, address(this), totalAmount);
 
         for (uint256 i; i < swaps.length; i++) {
-            ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            IV3SwapRouter.ExactInputSingleParams memory params = IV3SwapRouter.ExactInputSingleParams({
                 tokenIn: address(USDC),
                 tokenOut: swaps[i].tokenOut,
                 fee: swaps[i].fee,
                 recipient: msg.sender,
-                deadline: block.timestamp,
                 amountIn: swaps[i].amountIn,
                 amountOutMinimum: 0,
                 sqrtPriceLimitX96: 0
             });
 
             UNI_V3_Router.exactInputSingle(params);
+
+            userTokens[msg.sender].push(swaps[i].tokenOut);
         }
 
         uint256 remainingAmount = USDC.balanceOf(address(this));
         if (remainingAmount > 0) {
             USDC.transfer(msg.sender, remainingAmount);
         }
+    }
+
+    function getUserTokens(address user) external view returns (UserTokens[] memory) {
+        UserTokens[] memory tokens = new UserTokens[](userTokens[user].length);
+        for (uint256 i = 0; i < userTokens[user].length; i++) {
+            address token = userTokens[user][i];
+            tokens[i] = UserTokens({
+                token: token,
+                decimals: IERC20Metadata(token).decimals(),
+                balance: IERC20(token).balanceOf(user)
+            });
+        }
+        return tokens;
     }
 }
--- dapp/src/app/portfolio/page.tsx
+++ dapp/src/app/portfolio/page.tsx
@@ -1,9 +1,40 @@
+'use client';
+
 import React from 'react';
 
+import { useAccount, useReadContract } from 'wagmi';
+
+import PortfolioOverview from '@/components/portfolio/overview';
+
+const CONTRACT_ADDRESS = '0x123'; // Replace with your contract address
+const getUserTokensABI = [
+  {
+    type: 'function',
+    name: 'getUserTokens',
+    inputs: [{ name: 'user', type: 'address', internalType: 'address' }],
+    outputs: [
+      {
+        name: '',
+        type: 'tuple[]',
+        internalType: 'struct Bundler.UserTokens[]',
+        components: [
+          { name: 'token', type: 'address', internalType: 'address' },
+          { name: 'decimals', type: 'uint8', internalType: 'uint8' },
+          { name: 'balance', type: 'uint256', internalType: 'uint256' }
+        ]
+      }
+    ],
+    stateMutability: 'view'
+  }
+] as const;
 export default function PortfolioPage() {
-  return (
-    <div className='flex h-full w-full items-center justify-center'>
-      <h1 className='text-2xl'>Portfolio page</h1>
-    </div>
-  );
+  const { address } = useAccount();
+  const { data } = useReadContract({
+    address: CONTRACT_ADDRESS,
+    abi: getUserTokensABI,
+    functionName: 'getUserTokens',
+    args: [address || '0x']
+  });
+
+  return <PortfolioOverview />;
 }
